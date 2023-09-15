d6b83e8eb9a561acf8dd2659967b777512eb60f4
Jorge Pacheco <sofitech@me.com>
2023-09-15 14:48:44 -0400

    Actualizado

--- proyecto_integrador2.py
+++ proyecto_integrador2.py
@@ -1,1 +1,22 @@
-#hy q empezar
+import msvcrt
+
+def main():
+    print("Presiona cualquier tecla para mostrarla. Presiona ↑ para salir.")
+    
+    while True:
+        key = msvcrt.getch().decode('utf-8')
+        
+        if key == 'Alt+72':  # El código de flecha es '\x1b' (Escape)
+            arrow_key = msvcrt.getch().decode('utf-8')
+            if arrow_key == '[':
+                direction = msvcrt.getch().decode('utf-8')
+                if direction == 'A':  # Flecha hacia arriba
+                    print("¡Tecla de flecha hacia arriba presionada! Saliendo del programa.")
+                    break
+            else:
+                print(f"Tecla presionada: {key}")
+        else:
+            print(f"Tecla presionada: {key}")
+
+if __name__ == "__main__":
+    main()
